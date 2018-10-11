7be7f0ee8a56c6e25fb54bab2d0d83b13cb32011
Vasanchai <59070156@it.kmitl.ac.th>
2018-10-11 22:09:13 +0700

    test

---
+++ MobileApplication/app/src/main/java/com/example/blooddonorapp/blooddonorrecorder/LoginFragment.java
@@ -0,0 +1,23 @@
+package com.example.blooddonorapp.blooddonorrecorder;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}

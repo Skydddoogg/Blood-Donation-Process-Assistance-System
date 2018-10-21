0092424de428c133f57296a638c6c5a105de3b2e
Nawinx <59070088@kmitl.ac.th>
2018-10-21 17:39:45 +0700

    Create frontend (app) folder

---
+++ fronted application/BloodDonorFrontend/app/src/main/java/com/nawinc27/mac/blooddonorfrontend/LoginFragment.java
@@ -0,0 +1,33 @@
+package com.nawinc27.mac.blooddonorfrontend;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginFragment extends Fragment{
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView login_btn = getView().findViewById(R.id.login_loginBtn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new MainFragment()).addToBackStack(null).commit();
+            }
+        });
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container,false);
+    }
+}

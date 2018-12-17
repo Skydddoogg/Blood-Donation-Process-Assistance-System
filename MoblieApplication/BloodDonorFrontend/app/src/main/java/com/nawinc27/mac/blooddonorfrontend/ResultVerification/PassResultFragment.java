833a2f70aa60115293f436b6fb19ed756ce53c78
nawinest <nawinc.work@gmail.com>
2018-12-17 23:51:50 +0700

    Add Pass/Reject.xml pages

---
+++ MoblieApplication/BloodDonorFrontend/app/src/main/java/com/nawinc27/mac/blooddonorfrontend/ResultVerification/PassResultFragment.java
@@ -0,0 +1,24 @@
+package com.nawinc27.mac.blooddonorfrontend.ResultVerification;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nawinc27.mac.blooddonorfrontend.R;
+
+public class PassResultFragment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pass_verified , container , false);
+    }
+}
